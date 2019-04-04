62c81ac0cdc8ff9899a85ab00bcc582cc33a1687
Peter Granderson <solace@fastmail.fm>
2019-04-03 23:50:09 -0500

    initial

---
+++ precache-manifest.758b3def4822341dba514333c82348dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4d67becd5268a51ef77",
+    "url": "/static/js/main.386ed815.chunk.js"
+  },
+  {
+    "revision": "261937d83f7872a9f02f",
+    "url": "/static/js/2.79354598.chunk.js"
+  },
+  {
+    "revision": "e4d67becd5268a51ef77",
+    "url": "/static/css/main.f0e6e24f.chunk.css"
+  },
+  {
+    "revision": "5a8a2dcd3171630e7874438ba0f9370e",
+    "url": "/index.html"
+  }
+];
